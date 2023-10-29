7b08743a879225bfdefd0f4701c36114c57121c8
daniele.castrovinci <daniele.castrovinci@atos.net>
2023-10-29 12:24:45 +0100

    uspref

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -7,10 +7,20 @@
         </b-nav-item>
         <b-nav-item>
           <router-link v-if="!isAuthenticated" to="/register">REGISTER</router-link>
-          </b-nav-item>
+        </b-nav-item>
         <b-nav-item>
           <router-link v-if="isAuthenticated" to="/dashboard">DASHBOARD</router-link>
         </b-nav-item>
+        
+        <!-- Dynamic Menu Items -->
+        
+          <div v-for="item in visibleItems" :key="item.route">
+            <b-nav-item >
+              <router-link v-if="isAuthenticated" :to="item.route">{{ item.title }} </router-link>
+              <b-button v-if="isAuthenticated" @click="updateUserPreference(item)" v-model="item.visible" variant="danger" size="sm">X</b-button>
+            </b-nav-item>
+          </div>
+       
         
         <button v-if="isAuthenticated" @click="logout">Logout</button>
       </b-nav>
@@ -21,20 +31,45 @@
 
 <script>
 import menubar from "../data/menubar.json";
+
 export default {
-   props: {
-    isAuthenticated: Boolean, // Dichiarazione della prop isAuthenticated
+  props: {
+    isAuthenticated: Boolean,
     logout: Function,
   },
   data() {
     return {
-      model: 1,
-      name: "Menu",
       items: menubar.items,
-    }
-  }
-}
+    };
+  },
+  computed: {
+    visibleItems() {
+      return this.items.filter((item) => item.visible);
+    },
+  },
+  methods: {
+    updateUserPreference(item) {
+      // Toggle the visibility of the clicked item
+      item.visible = !item.visible;
+      // Update user preferences in localStorage
+      const userPreferences = this.items.reduce((prefs, item) => {
+        prefs[item.route] = item.visible;
+        return prefs;
+      }, {});
+      localStorage.setItem("userPreferences", JSON.stringify(userPreferences));
+    },
+  },
+  created() {
+    const userPreferences = JSON.parse(localStorage.getItem("userPreferences")) || {};
+    // Initialize the items based on user preferences in localStorage
+    this.items = menubar.items.map((item) => ({
+      ...item,
+      visible: userPreferences[item.route] !== false,
+    }));
+  },
+};
 </script>
+
 <style scoped>
 .border {
   border: 2px dashed orange;
